ef1b96ddcca2e60190b5fe8ac21db3f19989c9f2
Sher Wali <sherwal@gmail.com>
2021-05-26 21:22:21 +0500

    folder 3 lecture 6 working

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/user.js')
 
-
+const { errorHandler } = require('../helpers/dbErrorHandler.js')
 
 
 exports.signup = (req, res) => {
@@ -10,10 +10,11 @@ const user = new User(req.body)
 user.save((err, user) => {
     if(err) {
         return res.status(400).json({
-            err
+            err: errorHandler(err)
         })
     }
-
+        user.salt = undefined
+        user.hashed_password = undefined
     res.json({
         user
     })
